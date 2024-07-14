de4de937b9e62647f4cb6c6f45cb68b74b9168f9
EuanParr <53079444+EuanParr@users.noreply.github.com>
2024-07-14 15:45:29 +0100

    added freshsym, which returns a fresh symbol w.r.t. the given expression and environment

--- Elfen.hs
+++ Elfen.hs
@@ -188,7 +188,19 @@ specialOperators = Map.fromList
                     ("letrec1", fixOperator),
                     ("letrec2", fixOpTwo),
                     ("letrec", mutualFixOperator),
-                    ("apply", (\(Cons f (Cons xs Nil)) env -> eval f env >>= \f' -> evalList xs env >>= \xs' -> apply f' xs'))]
+                    ("apply", (\(Cons f (Cons xs Nil)) env -> eval f env >>= \f' -> evalList xs env >>= \xs' -> apply f' xs')),
+                    ("freshsym", freshSym)]
+
+freshSym :: Value -> Environment -> M Value
+freshSym expr env = pure $ Symbol $ sym $ fresh 0 $ add expr (Map.union env $ Map.fromList $ map (\k -> (Term k, Nil)) $ Map.keys specialOperators) where
+  add (Symbol s) env' = Map.insert (Term s) Nil env'
+  add (Cons x y) env' = add x $ add y env'
+  add _ env' = env'
+  fresh n env'
+    | Map.member (Term $ sym n) env' = fresh (n + 1) env'
+    | Map.member (Macro $ sym n) env' = fresh (n + 1) env'
+    | otherwise = n
+  sym n = "fresh" ++ show n
 
 fixOperator :: Value -> Environment -> M Value
 fixOperator (Cons (Cons (Symbol s) (Cons v Nil)) (Cons body Nil)) e = do
